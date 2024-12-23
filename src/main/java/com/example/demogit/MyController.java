bd3434c1c9b185353c4d74dd66863c59d61cd4d5
vasia20330 <vasiachernobay20330@gmail.com>
2024-12-23 16:16:53 +0200

    change

--- src/main/java/com/example/demogit/MyController.java
+++ src/main/java/com/example/demogit/MyController.java
@@ -1,4 +1,14 @@
 package com.example.demogit;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MyController {
+
+    @GetMapping("/")
+    public  String sayHello(){
+        return  "index";
+    }
 }
